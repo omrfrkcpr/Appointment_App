0e8b41ed948e4878aa775b12a9f449552a585243
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-03-05 17:15:21 +0100

    add project components + assets + update data.js

--- src/helpers/data.js
+++ src/helpers/data.js
@@ -1,45 +1,59 @@
 const Data = [
-    {
-      id: 1,
-      text: "Yavuz Selim",
-      day: "Dec 12th at 2:30pm",
-      bittiMi: false,
-      doktor: "DR Ayse Okur",
-    },
-    {
-      id: 2,
-      text: "Mihrimah Sultan",
-      day: "Dec 13th at 1:30pm",
-      bittiMi: true,
-      doktor: "DR Fatma Adil",
-    },
-    {
-      id: 3,
-      text: "Fatih Mehmet",
-      day: "Dec 14th at 3:00pm",
-      bittiMi: false,
-      doktor: "DR Oya Başar",
-    },
-    {
-      id: 4,
-      text: "İpek Bilir",
-      day: "Dec 12th at 2:30pm",
-      bittiMi: false,
-      doktor: "DR Ahmet Bilen",
-    },
-    {
-      id: 5,
-      text: "Haluk Yilmaz",
-      day: "Dec 13th at 1:30pm",
-      bittiMi: true,
-      doktor: "DR Ahmet Bilen",
-    },
-    {
-      id: 6,
-      text: "Ayşe Güler",
-      day: "Dec 14th at 3:00pm",
-      bittiMi: false,
-      doktor: "DR Oya Başar",
-    },
-  ];
-  export default Data;
+  {
+    id: 1,
+    patient: "Yavuz Selim",
+    appointment: "Dec 12th at 2:30pm",
+    isCompleted: false,
+    doctor: "Dr. Akari Yoshino",
+  },
+  {
+    id: 2,
+    patient: "Mihrimah Sultan",
+    appointment: "Dec 13th at 1:30pm",
+    isCompleted: true,
+    doctor: "Dr. Alena Mangold",
+  },
+  {
+    id: 3,
+    patient: "Fatih Mehmet",
+    appointment: "Dec 14th at 3:00pm",
+    isCompleted: false,
+    doctor: "Dr. Lian Kurzmann",
+  },
+  {
+    id: 4,
+    patient: "İpek Bilir",
+    appointment: "Dec 12th at 2:30pm",
+    isCompleted: false,
+    doctor: "Dr. Benjamin Daley",
+  },
+  {
+    id: 5,
+    patient: "Haluk Yilmaz",
+    appointment: "Dec 13th at 1:30pm",
+    isCompleted: true,
+    doctor: "Dr. Eva Manz",
+  },
+  {
+    id: 6,
+    patient: "Ayşe Güler",
+    appointment: "Dec 14th at 3:00pm",
+    isCompleted: false,
+    doctor: "Dr. Theodor MacClellan",
+  },
+  {
+    id: 7,
+    patient: "Ayşe Güler",
+    appointment: "Dec 14th at 3:00pm",
+    isCompleted: false,
+    doctor: "Dr. Nari Chung",
+  },
+  {
+    id: 8,
+    patient: "Ayşe Güler",
+    appointment: "Dec 14th at 3:00pm",
+    isCompleted: false,
+    doctor: "Dr. Wulf Gabler",
+  },
+];
+export default Data;
